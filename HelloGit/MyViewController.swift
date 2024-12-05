d7c700070847181d7ec53a4917397cfa4dab4d2e
willy <willy548798@gamil.com>
2024-12-05 21:04:59 +0800

    第一次測試

---
+++ HelloGit/MyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MyViewController.swift
+//  HelloGit
+//
+//  Created by Willy Hsu on 2024/12/5.
+//
+
+import UIKit
+
+class MyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
